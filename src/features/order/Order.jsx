f22dc12a3cb56d0e26384dd640582ca3688ba231
Raoul <grnraoul@gmail.com>
2024-04-30 14:55:42 +0300

    configured prettier

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,61 +1,66 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
-import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from 'react-router-dom'
+import { getOrder } from '../../services/apiRestaurant'
 import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
+    calcMinutesLeft,
+    formatCurrency,
+    formatDate,
+} from '../../utils/helpers'
 
 function Order() {
-  const order = useLoaderData();
+    const order = useLoaderData()
 
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
+    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+    const {
+        id,
+        status,
+        priority,
+        priorityPrice,
+        orderPrice,
+        estimatedDelivery,
+        cart,
+    } = order
 
-  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+    const deliveryIn = calcMinutesLeft(estimatedDelivery)
 
-  return (
-    <div>
-      <div>
-        <h2>Status</h2>
-
+    return (
         <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+            <div>
+                <h2>Status</h2>
+
+                <div>
+                    {priority && <span>Priority</span>}
+                    <span>{status} order</span>
+                </div>
+            </div>
+
+            <div>
+                <p>
+                    {deliveryIn >= 0
+                        ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+                        : 'Order should have arrived'}
+                </p>
+                <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+            </div>
+
+            <div>
+                <p>Price pizza: {formatCurrency(orderPrice)}</p>
+                {priority && (
+                    <p>Price priority: {formatCurrency(priorityPrice)}</p>
+                )}
+                <p>
+                    To pay on delivery:{' '}
+                    {formatCurrency(orderPrice + priorityPrice)}
+                </p>
+            </div>
         </div>
-      </div>
-
-      <div>
-        <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
-        </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-      </div>
-
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
-      </div>
-    </div>
-  );
+    )
 }
 
 export async function loader({ params }) {
-  const order = await getOrder(params.orderId);
-  return order;
+    const order = await getOrder(params.orderId)
+    return order
 }
 
-export default Order;
+export default Order
