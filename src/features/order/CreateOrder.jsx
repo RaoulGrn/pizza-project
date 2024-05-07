d605dfefacc7a719960b7776492870544b2911c0
Raoul <grnraoul@gmail.com>
2024-05-07 14:35:44 +0300

    Customizing the order, createOrder and orderItem components, with some small changes to the custom button and to the Header file

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
-import { createOrder } from '../../services/apiRestaurant'
-import Button from '../../ui/Button'
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
         str
-    )
+    );
 
 const fakeCart = [
     {
@@ -31,51 +31,57 @@ const fakeCart = [
         unitPrice: 15,
         totalPrice: 15,
     },
-]
+];
 
 function CreateOrder() {
-    const navigation = useNavigation()
-    const isSubmitting = navigation.state === 'submitting'
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
 
-    const formErrors = useActionData()
+    const formErrors = useActionData();
 
     // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart
+    const cart = fakeCart;
 
     return (
-        <div>
-            <h2>Ready to order? Let`&apos;`s go!</h2>
+        <div className="px-4 py-6">
+            <h2 className="mb-8 text-xl font-semibold">
+                Ready to order? Let&apos;s go!
+            </h2>
 
             <Form method="POST" action="">
-                <div>
-                    <label>First Name</label>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">First Name</label>
                     <input
-                        className="input"
+                        className="input grow"
                         type="text"
                         name="customer"
                         required
                     />
                 </div>
 
-                <div>
-                    <label>Phone number</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Phone number</label>
+                    <div className="grow">
                         <input
-                            className="input"
+                            className="input w-full"
                             type="tel"
                             name="phone"
                             required
                         />
                     </div>
 
-                    {formErrors?.phone && <p>{formErrors.phone}</p>}
+                    {formErrors?.phone && (
+                        <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                            {formErrors.phone}
+                        </p>
+                    )}
                 </div>
 
-                <div>
-                    <label>Address</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Address</label>
+                    <div className="grow">
                         <input
-                            className="input"
+                            className="input w-full"
                             type="text"
                             name="address"
                             required
@@ -83,7 +89,7 @@ function CreateOrder() {
                     </div>
                 </div>
 
-                <div>
+                <div className="item-center mb-12 flex gap-5">
                     <input
                         className="h-6 w-6 accent-teal-400 focus:outline-none focus:ring focus:ring-teal-400 focus:ring-offset-2"
                         type="checkbox"
@@ -92,7 +98,7 @@ function CreateOrder() {
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor="priority">
+                    <label htmlFor="priority" className="font-medium">
                         Want to yo give your order priority?
                     </label>
                 </div>
@@ -109,31 +115,31 @@ function CreateOrder() {
                 </div>
             </Form>
         </div>
-    )
+    );
 }
 
 export async function action({ request }) {
-    const formData = await request.formData()
-    const data = Object.fromEntries(formData)
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
 
     const order = {
         ...data,
         cart: JSON.parse(data.cart),
         priority: data.priority === 'on',
-    }
+    };
 
-    const errors = {}
+    const errors = {};
     if (!isValidPhone(order.phone))
         errors.phone =
-            'Please give us your correct phone number. We might need it to contact you.'
+            'Please give us your correct phone number. We might need it to contact you.';
 
-    if (Object.keys(errors).length > 0) return errors
+    if (Object.keys(errors).length > 0) return errors;
 
     // If everything is okay, create new order and redirect
     // const newOrder = await createOrder(order)
 
     // return redirect(`/order/${newOrder.id}`)
-    return null
+    return null;
 }
 
-export default CreateOrder
+export default CreateOrder;
