5d75d46a250443bb3161a870312a69c8b2d22c7d
Raoul <grnraoul@gmail.com>
2024-05-12 18:51:33 +0300

    Implementing the functionality for the geolocation feature

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,27 +3,11 @@ import { getAddress } from '../../services/apiGeocoding';
 
 const initialState = {
     username: '',
+    status: 'idle',
+    position: {},
+    address: '',
+    error: '',
 };
-
-const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        updateName(state, action) {
-            state.username = action.payload;
-        },
-    },
-});
-
-export const { updateName } = userSlice.actions;
-
-export default userSlice.reducer;
-
-function getPosition() {
-    return new Promise(function (resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-}
 
 export const fetchAddress = createAsyncThunk(
     'user/fetchAddress',
@@ -39,6 +23,41 @@ export const fetchAddress = createAsyncThunk(
         const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
         // 3) Then we return an object with the data that we are interested in
+        // Payload of the fulfilled state
         return { position, address };
     }
 );
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        updateName(state, action) {
+            state.username = action.payload;
+        },
+    },
+    extraReducers: (builder) =>
+        builder
+            .addCase(fetchAddress.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchAddress.fulfilled, (state, action) => {
+                state.position = action.payload.position;
+                state.address = action.payload.address;
+                state.state = 'idle';
+            })
+            .addCase(fetchAddress.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.error.message;
+            }),
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
+
+function getPosition() {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
